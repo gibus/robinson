18165a71153ab961c9685151d06d3319c074bf06
Bachir Soussi Chiadmi <bachir@g-u-i.net>
2014-03-10 21:57:49 +0100

    Thematiques : lettrage image

--- templates/node--thematique--stream.tpl.php
+++ templates/node--thematique--stream.tpl.php
@@ -1,6 +1,9 @@
 
 <?php print render($title_prefix); ?>
-<h2 class="thema-title"<?php print $title_attributes; ?>><span class="txt"><?php print $title; ?></span></h2>
+<h2 class="thema-title"<?php print $title_attributes; ?>>
+  <span class="txt"><?php print $title; ?></span>
+  <?php print render($content['field_titre_lettrage']); ?>
+</h2>
 <?php print render($title_suffix); ?>
 
 <section class="content">
@@ -8,6 +11,7 @@
   // dsm($content, 'content');
   hide($content['comments']);
   hide($content['links']);
+  hide($content['field_titre_lettrage']);
   print render($content);
 
   // print render($content['field_video']);
