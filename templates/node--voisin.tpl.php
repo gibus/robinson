911703bbf30b7f1efc199cd0394f7dc894e02ad5
Bachir Soussi Chiadmi <bachir@g-u-i.net>
2014-05-19 17:17:26 +0200

    fixed voisin images display

--- templates/node--voisin.tpl.php
+++ templates/node--voisin.tpl.php
@@ -1,6 +1,9 @@
 <?php
+
 $voisin_classes = array("voisin");
 $voisin_classes[] = "voisin-".$node->nid;
+$media_term = taxonomy_term_load($node->field_type_media['und'][0]['tid']);
+$voisin_classes[] = stripAccents($media_term->name);
 $voisin_classes[] = "voisin-media-".$node->field_type_media['und'][0]['tid'];
 $voisin_classes[] = str_replace('voisin-', '', str_replace('_', '-', $view_mode));
 
