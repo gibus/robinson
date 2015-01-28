1f869b4f7e212a8ef278e480ad79ae691bc83e50
Julien G <julien@g-u-i.net>
2015-01-28 22:03:54 +0100

    up

--- tpl/robinsonmod-ressources.tpl.php
+++ tpl/robinsonmod-ressources.tpl.php
@@ -8,7 +8,11 @@
   <article class="item-list col-xs-12 col-sm-6 col-md-4 col-lg-3">
     <a href="<?= current_path().'/'.$node->nid ?>">
       <h3 class="item-title"><?= $node->title ?></h3>
-      <?php $field_description = field_view_field('node', $node, 'field_description', array("label"=>"hidden", 'type'=>'trimmed')); ?>
+      <?php $field_description = field_view_field('node', $node, 'field_description', array(
+        "label"=>"hidden",
+        'type' => 'text_summary_or_trimmed',
+        'settings'=>array('trim_length' => 150),
+      )); ?>
       <?= render( $field_description ) ?>
     </a>
   </article>
