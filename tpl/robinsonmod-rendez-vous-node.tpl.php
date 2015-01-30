95e9290371b324dc710c3dffbaf8b3392839d179
Julien G <julien@g-u-i.net>
2015-01-30 19:01:57 +0100

    fix [test]

--- tpl/robinsonmod-rendez-vous-node.tpl.php
+++ tpl/robinsonmod-rendez-vous-node.tpl.php
@@ -8,16 +8,14 @@
 
   <article class="node node-<?= $node->nid ?> col-xs-12 col-sm-10 col-md-8 col-lg-6">
     <?php
-    // $fieldvalues = field_get_items('node', $node, 'field_prog_plage');
-    // foreach ($fieldvalues as $fieldvalue) :
-    //   $tstart = timestampDay( $fieldvalue['value'], TRUE );
-    //   $format = 'H:i';
-    //   $start  = format_date( $fieldvalue['value'],  'custom', $format );
-    //   $end    = format_date( $fieldvalue['value2'], 'custom', $format );
-    //   break;
-    // endforeach;
-    $start = "blabla";
-    $end = "blabla";
+    $fieldvalues = field_get_items('node', $node, 'field_prog_plage');
+    foreach ($fieldvalues as $fieldvalue) :
+      $tstart = timestampDay( $fieldvalue['value'], TRUE );
+      $format = 'H:i';
+      $start  = format_date( $fieldvalue['value'],  'custom', $format );
+      $end    = format_date( $fieldvalue['value2'], 'custom', $format );
+      break;
+    endforeach;
     $field_prog_plage  = field_view_field('node', $node, 'field_prog_plage',  array("label"=>"hidden"));
     $field_description = field_view_field('node', $node, 'field_description', array("label"=>"hidden"));
     ?>
