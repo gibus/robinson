6393cba5b193a847a92cf26a4f3f60f58332ce96
Julien G <julien@g-u-i.net>
2015-02-04 14:25:12 +0100

    red for program + homepage message

--- preprocess/node.pre.php
+++ preprocess/node.pre.php
@@ -3,6 +3,9 @@
 // dsm("3");
 // dsm($vars);
 
+if ($vars["is_front"])
+  $vars["theme_hook_suggestions"][] = "node__front";
+
 $vars['theme_hook_suggestions'][] = 'node__'.$vars['view_mode'];
 $vars['theme_hook_suggestions'][] = 'node__' . $vars['type'] . '__' . $vars['view_mode'];
 $vars['classes_array'][] = 'view-mode-' . $vars['view_mode'];
