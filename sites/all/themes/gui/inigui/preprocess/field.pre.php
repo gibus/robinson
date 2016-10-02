4ef1a91137b037e3b8bcbe85a90f89bede49cc37
gibus <gibus@april.org>
2016-10-02 17:47:35 +0200

    Remove gui themes, IMPLIES admin theme to be set to Rubik.

--- sites/all/themes/gui/inigui/preprocess/field.pre.php
+++
@@ -1,8 +0,0 @@
-<?php 
-
-//dsm($vars);
-
-$vars['theme_hook_suggestions'][] = 'field__' . $vars['element']['#view_mode'];
-$vars['theme_hook_suggestions'][] = 'field__' . $vars['element']['#field_type'] . '__' . $vars['element']['#view_mode'];
-$vars['theme_hook_suggestions'][] = 'field__' . $vars['element']['#field_name'] . '__' . $vars['element']['#view_mode'];
-
